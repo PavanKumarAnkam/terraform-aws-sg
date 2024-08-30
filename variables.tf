9f5fcfb3b6aac652fe422c8e29031a940c74dc65
PavanKumarAnkam <pa1kumarankam@gmail.com>
2024-08-30 20:00:49 +0530

    latest

--- variables.tf
+++ variables.tf
@@ -1,19 +1,46 @@
 variable "project_name" {
   type = string
-  default = "expense"
 }
+
 variable "environment" {
-    type = string
-    default = "dev"
+  type = string
 }
-# variable "sg_name" {
-#   type = string
-#   default = "${project_name}-${}-${}"
-# }
 
 variable "sg_name" {
   type = string
 }
+
 variable "sg_description" {
   type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "ingress_rules" {
+  type    = list
+  default = []
+}
+
+variable "outbound_rules" {
+  type = list
+  default = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1" # all protocols
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "common_tags" {
+  type    = map
+  default = {}
+}
+
+variable "sg_tags" {
+  type    = map
+  default = {}
 }
